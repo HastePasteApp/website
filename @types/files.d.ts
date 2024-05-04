e098653dda72d09c7b591f6a1a117d27477ce598
Barış DEMİRCİ <hi@338.rocks>
2024-05-04 10:29:42 +0300

    Repo cleanup for v3

--- @types/files.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "*.yaml" {
-	const yaml: { [prop: string]: string | string[] };
-	export default yaml;
-}
-
-declare module "*.yml" {
-	const yaml: { [prop: string]: string | string[] };
-	export default yaml;
-}
